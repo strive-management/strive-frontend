94a8cd32966c044815a7101a498ca454dfa2d8ec
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-05 16:07:29 +0900

    update button color

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -36,7 +36,7 @@ export default function Register() {
           </div>
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-gray-500 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Login
           </button>
