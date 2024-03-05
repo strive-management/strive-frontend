ea3c6a531971dcfb2ecaae2931522e7c0238c8c1
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-05 16:08:30 +0900

    firebase install

---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbdxXF20Yu33dKfrrBQ0xUiDdPG_tU8PI',
+  authDomain: 'strive-dac54.firebaseapp.com',
+  projectId: 'strive-dac54',
+  storageBucket: 'strive-dac54.appspot.com',
+  messagingSenderId: '46093948887',
+  appId: '1:46093948887:web:84f7fc6b4902f517de5d92',
+  measurementId: 'G-941NTFJXLE',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default { auth, app };
