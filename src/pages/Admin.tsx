41252c5a6acfb49a3d8e48010d3cdfc9aa5164f3
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-07 18:14:55 +0900

    update darkmode color

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -10,13 +10,13 @@ export default function Admin() {
         <Sidebar />
         <div className="flex items-center justify-around ml-64 h-min mt-5 pb-2 gap-20 border-b-2 ">
           <div className="">
-            <h1>ADMIN</h1>
+            <h1 className="font-medium">ADMIN</h1>
           </div>
           <div className="flex items-center">
             <input
               type="text"
               placeholder="Search"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full  block w-full p-1.5"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full  block w-full p-1.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-200 dark:text-white "
             />
           </div>
         </div>
@@ -64,6 +64,25 @@ export default function Admin() {
             <div className="w-1/2">
               <Input type={'text'} placeholder="Address line 2" />
             </div>
+          </div>
+          <label className="font-medium text-lg">Role</label>
+          <div className="flex justify-around mt-5">
+            <div className="">
+              <Label text={'Manager'} />
+              <Input type={'text'} placeholder="Select manager" />
+            </div>
+            <div>
+              <Label text={'Location'} />
+              <Input type={'text'} placeholder="Select Location" />
+            </div>
+          </div>
+          <div className="flex justify-end mx-28 ">
+            <button
+              type="submit"
+              className="mt-10 text-black bg-blue-200 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:text-white dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              send
+            </button>
           </div>
         </main>
       </div>
