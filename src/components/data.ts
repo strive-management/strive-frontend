57b7e4d22f74a4383a7cf685bea2c8b8fbbc6bae
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-08 15:34:01 +0900

    add donuts fake data

--- src/components/data.ts
+++ src/components/data.ts
@@ -30,3 +30,10 @@ export const data = [
     Tomatoes: 2290,
   },
 ];
+
+export const data2 = [
+  { name: 'USA', value: 400, color: 'indigo.6' },
+  { name: 'India', value: 300, color: 'yellow.6' },
+  { name: 'Japan', value: 100, color: 'teal.6' },
+  { name: 'Other', value: 200, color: 'gray.6' },
+];
