0c63f590ce437586246fc0ba71ab873251ad924c
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2024-03-13 12:52:20 +0900

    Revert "ui glitch: links not working"

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "./SideBar";
+import Navbar from "./SideBar";
 
 type LayoutProps = {
     children: React.ReactNode;
@@ -9,7 +9,7 @@ type LayoutProps = {
   const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
       <div>
-        <Sidebar />
+        <Navbar />
         <div>{children}</div>
       </div>
     );
