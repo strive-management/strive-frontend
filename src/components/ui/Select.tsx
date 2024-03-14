86a63cf17bd3b1992b2c449a93d26f1f337b7fa4
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-14 20:46:58 +0900

    add some logic to the get a input as option

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,9 +1,15 @@
+interface Option {
+  id: number;
+  [key: string]: string | number;
+}
 interface SelectProps {
   name: string;
   value: string | [];
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  options: { id: number; name: string }[];
+  options: Option[];
   defaultOption?: string;
+  includeAddNew?: boolean; // New prop to conditionally include an "Add new..." option
+  onAddNew?: () => void; // Handler for when "Add new..." is selected
 }
 
 const Select: React.FC<SelectProps> = ({
@@ -12,12 +18,22 @@ const Select: React.FC<SelectProps> = ({
   onChange,
   options,
   defaultOption = 'Select option',
+  includeAddNew = false,
+  onAddNew,
 }) => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === 'Add new...') {
+      onAddNew?.();
+    } else {
+      onChange(e);
+    }
+  };
+
   return (
     <select
       name={name}
       value={value}
-      onChange={onChange}
+      onChange={handleOnChange}
       className="select-class-names"
     >
       <option value="">{defaultOption}</option>
@@ -26,6 +42,7 @@ const Select: React.FC<SelectProps> = ({
           {option.name}
         </option>
       ))}
+      {includeAddNew && <option value="Add new...">Add new...</option>}
     </select>
   );
 };
