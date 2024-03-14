c1aca3b1178e9a0de985c34201ed75121f84b5b9
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-14 14:05:14 +0900

    remove sidebar from Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import SideNavBar from "../components/NewNavBar";
-
+import React from 'react';
+import SideNavBar from '../components/SideNavBar';
 
 type LayoutProps = {
-    children: React.ReactNode;
-  };
+  children: React.ReactNode;
+};
 
-
-  const Layout: React.FC<LayoutProps> = ({ children }) => {
-    return (
-      <div>
-        <SideNavBar />
-        <div>{children}</div>
-      </div>
-    );
-  };
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <SideNavBar />
+      <div>{children}</div>
+    </div>
+  );
+};
 
 export default Layout;
