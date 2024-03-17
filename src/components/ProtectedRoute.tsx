31156de57e2c73168277e2d47314ae1a23de23f3
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-17 23:38:48 +0900

    feat: implement protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { currentUser } = useAuth();
+  const navigate = useNavigate();
+
+  if (!currentUser) {
+    return <Navigate to="/login" replace />;
+  }
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
