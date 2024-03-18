a0bd97bb6f165e125d4915d343b392b58c57cc4d
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-18 10:49:14 +0900

    remove usenavigate

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 interface ProtectedRouteProps {
   children: ReactNode;
