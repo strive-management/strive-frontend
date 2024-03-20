2627babb1b1d59dbf7374465168adc17d22a3c5c
ShizuokaTerrier <matt@squareoneJapan.com>
2024-03-20 14:45:40 +0900

    feat: update dates, add edit delete buttons to schedule, update dependency array

--- src/pages/ScheduleView.tsx
+++ src/pages/ScheduleView.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState, ChangeEvent } from "react";
-import axios from "axios";
+import { useEffect, useState, ChangeEvent } from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import EditModal from '../components/EditModal';
+import DeleteUserModal from '../components/DeleteUserModal';
 
 interface ScheduleInfo {
   id: number;
@@ -24,9 +27,14 @@ const ScheduleView = () => {
   >([]);
 
   const [range, setRange] = useState<range>({
-    from: "",
-    until: "",
+    from: '',
+    until: '',
   });
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   // const headers = Object.keys(scheduleInformation[0] || {});
   const rows = scheduleInformation.map((item) => {
@@ -42,21 +50,36 @@ const ScheduleView = () => {
         console.log(error);
       }
     };
-    console.log(scheduleInformation);
     fetchScheduleData();
   }, []);
 
   function handleRange(e: ChangeEvent<HTMLInputElement>) {
     setRange({ ...range, [e.target.name]: `${e.target.value}T00:00:00.000Z` });
   }
-  console.log(range);
+
+  async function handleDelete(
+    e: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+    id: number
+  ) {
+    e.preventDefault();
+    closeModal();
+    await axios
+      .delete(`${LOCALDB_URL}schedules/${id}}`)
+      .then(function (response) {
+        console.log(response);
+        const newData = scheduleInformation.filter((item) => item.id !== id);
+        setScheduleInformation(newData);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
   return (
     <>
-      <div className="flex flex-col w-full overflow-auto">
-      <div className="top-20 p-5 sm:p-10 mt-20 sm:mt-10">
-
-      {/* <div className="flex flex-row place-content-start pl-[300px] items-center bg-gray-300 w-full h-20">
+      <div className='flex flex-col w-full overflow-auto'>
+        <div className='top-20 p-5 sm:p-10 mt-20 sm:mt-10'>
+          {/* <div className="flex flex-row place-content-start pl-[300px] items-center bg-gray-300 w-full h-20">
         <h1
           style={{ fontFamily: "'Lato', sans-serif" }}
           className="text-gray-700 text-xl place-content-center"
@@ -65,84 +88,105 @@ const ScheduleView = () => {
         </h1>
       </div> */}
 
-      
-
-        <div className="flex flex-col p-5 sm:p-10 mt-10 border-2 border-gray-300 dark:border-gray-300 rounded-xl">
-          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-              <div className="overflow-hidden mb-20">
-                <table className="min-w-full text-left text-sm font-light text-surface dark:text-white">
-                  <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
-                    <tr>
-                      {/* {headers.map((header) => (
+          <div className='flex flex-col p-5 sm:p-10 mt-10 border-2 border-gray-300 dark:border-gray-300 rounded-xl'>
+            <div className='overflow-x-auto sm:-mx-6 lg:-mx-8'>
+              <div className='inline-block min-w-full py-2 sm:px-6 lg:px-8'>
+                <div className='overflow-hidden mb-20'>
+                  <table className='min-w-full text-left text-sm font-light text-surface dark:text-white'>
+                    <thead className='border-b border-neutral-200 font-medium dark:border-white/10'>
+                      <tr>
+                        {/* {headers.map((header) => (
                   <th key={header} >
                     {header}
                   </th>
                 ))} */}
-                      <th scope="col" className="px-6 py-4">
-                        ID
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Employee ID
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Date
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Available
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Scheduled Start
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Scheduled End
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Clock In
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Clock Out
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {rows
-                      .filter(
-                        (row) => row[2] >= range.from && row[2] <= range.until
-                      ) // Filter rows based on condition
-                      .map((row) => (
-                        <tr key={row[0]}>
-                          {row.map((cell, index) => (
-                            <td key={index}>{cell}</td>
-                          ))}
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </div>
-              <div className="flex flex-row gap-6 text-gray-700 dark:text-gray-300">
-                <h4>From</h4>
-                <input
-                  type="date"
-                  className="px-6 py-2 border rounded-lg bg-gray-50 dark:bg-gray-700"
-                  name="from"
-                  onChange={handleRange}
-                />
-                <h4>To</h4>
-                <input
-                  type="date"
-                  className="px-6 py-2 border rounded-lg bg-gray-50 dark:bg-gray-700"
-                  name="until"
-                  onChange={handleRange}
-                />
+
+                        <th scope='col' className='px-6 py-4'>
+                          Employee ID
+                        </th>
+                        <th scope='col' className='px-6 py-4'>
+                          Date
+                        </th>
+                        <th scope='col' className='px-6 py-4'>
+                          Available
+                        </th>
+                        <th scope='col' className='px-6 py-4'>
+                          Scheduled Start
+                        </th>
+                        <th scope='col' className='px-6 py-4'>
+                          Scheduled End
+                        </th>
+                        <th scope='col' className='px-6 py-4'>
+                          Clock In
+                        </th>
+                        <th scope='col' className='px-6 py-4'>
+                          Clock Out
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {rows
+                        .filter(
+                          (row) => row[2] >= range.from && row[2] <= range.until
+                        ) // Filter rows based on condition
+                        .map((row) => (
+                          <tr key={row[0]}>
+                            {row.map(
+                              (cell, index) =>
+                                index !== 0 &&
+                                index !== 6 && (
+                                  <td key={index}>
+                                    {typeof cell === 'string' &&
+                                    dayjs(cell).isValid()
+                                      ? dayjs(cell).format('YYYY/MM/DD')
+                                      : typeof cell === 'boolean'
+                                      ? cell.toString()
+                                      : cell}
+                                  </td>
+                                )
+                            )}{' '}
+                            <td>
+                              <EditModal id={row[0]} />
+                            </td>
+                            <td>
+                              <button
+                                className='inline-block rounded bg-blue-50 dark:bg-red-400 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-gray-700 shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong'
+                                onClick={() => openModal()}
+                              >
+                                Delete
+                              </button>
+                              <DeleteUserModal
+                                isOpen={isModalOpen}
+                                onClose={closeModal}
+                                onConfirm={(e) => handleDelete(e, row[0])}
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div className='flex flex-row gap-6 text-gray-700 dark:text-gray-300'>
+                  <h4>From</h4>
+                  <input
+                    type='date'
+                    className='px-6 py-2 border rounded-lg bg-gray-50 dark:bg-gray-700'
+                    name='from'
+                    onChange={handleRange}
+                  />
+                  <h4>To</h4>
+                  <input
+                    type='date'
+                    className='px-6 py-2 border rounded-lg bg-gray-50 dark:bg-gray-700'
+                    name='until'
+                    onChange={handleRange}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-      
-      
     </>
   );
 };
